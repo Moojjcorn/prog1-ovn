f68f7f581ad8566c55a5ce584f851afb66fc2fd3
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2020-08-31 14:45:08 +0200

    Update kap2test.py

--- kap2test.py
+++ kap2test.py
@@ -1,4 +1,4 @@
 tal1 = input('Vad är första talet?')
 räknesätt = input('Vilket räknesätt?')
-tal2 = input('Vadär andra talet?')
+tal2 = input('Vad är andra talet?')
 print('Ditt tal är',tal1, räknesätt, tal2)
