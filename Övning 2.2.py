662cbb11e383cfa9e6de74c4a26f330d4067d77d
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2020-09-23 10:58:49 +0200

    Update Övning 2.2.py

--- Övning 2.2.py
+++ Övning 2.2.py
@@ -1,9 +1,10 @@
-r = input=('Hur lång är radien? ') #Får input till variabeln r
+r = input('Hur lång är radien? ') #Får input till variabeln r
 
 r = float(r) # R är en variable som använder decimaler
+
 
 v = 4 * 3.14 * r * r * r # Formeln till en sfär
 
 v = v / 3 # fortsättning av formeln
 
-print(f'{v}') # printar svaret
+print(f'{v:.2f} Är Sfärens volym ') # printar svaret
