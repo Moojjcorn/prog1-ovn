0e42ec0e073d231630f6318093a4a72ed250c6bf
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2020-11-02 14:00:31 +0100

    Update while.py

--- while.py
+++ while.py
@@ -3,13 +3,12 @@ randomTal = random.randint(1,10)
 gissa = 0
 tries = 1
 while gissa != randomTal :
+    tries += 1
     gissa = float(input('Gissa på ett heltal mellan 1 och 10: '))
     if gissa < randomTal :
         print(f"{gissa} är för lite")
-        tries += 1
     elif gissa > randomTal :
         print(f"{gissa} är för mycket")
-        tries += 1
     else:
         print(f"{gissa} är rätt")
 print(f"👌 Grattis! Du gissade rätt number på {tries} antal försök!👌 ")
